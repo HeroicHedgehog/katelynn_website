46ab7774a43017b6725e49cf6ad7ac0d3c5dd295
Katelynn Romanowski <70578183+HeroicHedgehog@users.noreply.github.com>
2021-03-19 17:09:26 -0500

    Added name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          KATELYNN'S WEBSITE
         </p>
         <a
           className="App-link"
