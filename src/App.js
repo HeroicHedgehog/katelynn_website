afd06a995307a074c03732d3dd645b2396501b9b
Katelynn Romanowski <70578183+HeroicHedgehog@users.noreply.github.com>
2021-03-23 11:15:58 -0500

    more pictures

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import mazie from './Pictures/mazie.jpg';
+import neeko from './Pictures/neeko.jpg';
+import family from './Pictures/family.jpg';
+import together from './Pictures/together.jpg';
 import './App.css';
 
 function App() {
@@ -6,6 +9,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={mazie} className="App-logo" alt="logo" />
+        <img src={neeko} className="App-logo" alt="logo" />
+        <img src={family} className="App-logo" alt="logo" />
+        <img src={together} className="App-logo" alt="logo" />
         <p>
           KATELYNN'S WEBSITE
         </p>
